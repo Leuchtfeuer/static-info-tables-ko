c32e34335c96b4a4e92ac4df122e8c347902b8cc
verena.tesar <verena.tesar@udg.de>
2021-06-15 16:33:12 +0200

    [BUGFIX] fix typoscript-condition for TYPO3 >= V9.5

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,16 @@ defined('TYPO3_MODE') || die;
 call_user_func(
     function($extensionKey) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.typoscript">');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest.tsconfig">');
+
+        if (version_compare(TYPO3_branch, '9.5', '>=')) {
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+                '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
+            );
+        } else {
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+                '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest_prior_9.tsconfig">'
+            );
+        }
+
     }, \Bitmotion\StaticInfoTablesKo\Extension::EXTENSION_KEY
 );
