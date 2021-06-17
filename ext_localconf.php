fcaf004c0e06d0bcc1711647a7755a62f699b12d
verena.tesar <verena.tesar@udg.de>
2021-06-17 16:40:51 +0200

    [TASK] remove support for TYPO3 < V9

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,16 +4,7 @@ defined('TYPO3_MODE') || die;
 call_user_func(
     function($extensionKey) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.typoscript">');
-
-        if (version_compare(TYPO3_branch, '9.5', '>=')) {
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-                '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
-            );
-        } else {
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-                '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest_prior_9.tsconfig">'
-            );
-        }
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest.tsconfig">');
 
     }, \Bitmotion\StaticInfoTablesKo\Extension::EXTENSION_KEY
 );
